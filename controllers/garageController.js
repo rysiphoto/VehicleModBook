c08dabe3519b35871984ce73a7aa08f47906aa8f
Ryan Siverson <rcsskier@mac.com>
2020-06-08 15:02:32 -0600

    fixed file structure

--- controllers/garageController.js
+++ controllers/garageController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the VehiclesController
+// Defining methods for the GaragesController
 module.exports = {
   findAll: function (req, res) {
-    db.Vehicle
+    db.Garage
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function (req, res) {
-    db.Vehicle
+    db.Garage
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function (req, res) {
-    db.Vehicle
+    db.Garage
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function (req, res) {
-    db.Vehicle
+    db.Garage
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function (req, res) {
-    db.Vehicle
+    db.Garage
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
